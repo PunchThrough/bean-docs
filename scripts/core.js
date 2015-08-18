8bde13f84054873801dad11d8f744036e2891fc1
Matthew Lewis <matt@mplewis.com>
2015-08-18 17:09:27 -0500

    Clean up core config variables

--- scripts/core.js
+++ scripts/core.js
@@ -2,11 +2,17 @@ import Metalsmith from 'metalsmith'
 import Markdown from 'metalsmith-markdown'
 import Layouts from 'metalsmith-layouts'
 
-export default Metalsmith(__dirname)
-  .source('../src')
-  .destination('../build')
-  .use(Markdown())
-  .use(Layouts({
+const config = {
+  src: '../src',
+  dest: '../build',
+  layouts: {
     engine: 'handlebars',
     directory: '../layouts'
-  }))
+  }
+}
+
+export default Metalsmith(__dirname)
+  .source(config.src)
+  .destination(config.dest)
+  .use(Markdown())
+  .use(Layouts(config.layouts))
