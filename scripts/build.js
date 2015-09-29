660d75532d2ef7cf09549631dcfc03297951a64a
Matthew Lewis <matt@mplewis.com>
2015-09-29 14:07:17 -0500

    style: make build.js more readable

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,24 @@
-import rimraf from 'rimraf'
 import core from './core'
+import Rimraf from 'rimraf'
+import Yaml from 'js-yaml'
+import fs from 'fs'
+
+const config = Yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'))
+
+const onClear = (err) => {
+  if (err) throw err
+  console.log('Starting build')
+  core
+    // .use(YourPlugin(config.yourPlugin))
+    .build(onBuild)
+}
+
+const onBuild = (err) => {
+  if (err) throw err
+  else console.log('Build successful!')
+}
 
 export default function() {
   console.log('Deleting old build files')
-  rimraf('build', (err) => {
-    if (err) throw err
-    console.log('Starting build')
-    core.build(function(err) {
-      if (err) throw err
-      else console.log('Build successful!')
-    })
-  })
+  Rimraf('build', onClear)
 }
