aace2bf4ffcde20437a5f381078d9be81b1a8fa6
Matthew Lewis <matt@mplewis.com>
2015-12-21 14:55:02 -0600

    template: improve relative video helper to add max-width

--- templates/helpers/video_rel.js
+++ templates/helpers/video_rel.js
@@ -1,6 +1,16 @@
 'use strict'
 
-module.exports = (context, name) => {
-  let src = context.relativeRoot + '_assets/images/' + context.path.dir + '/' + context.path.name + '/' + name
-  return '<video class="guide-video" autoplay="autoplay" loop="loop"><source src="' + src + '" type="video/mp4"></video>'
+module.exports = (context, name, maxWidth) => {
+  let src = `${context.relativeRoot}_assets/images/${context.path.dir}/${context.path.name}/${name}`
+  let style = ''
+  if (maxWidth) {
+    style = `max-width: ${maxWidth};`
+  }
+  return (
+`<div class="guide-img-holder">
+  <a href="${src}" target="_blank">
+    <video class="guide-video" autoplay="autoplay" loop="loop" style="${style}"><source src="${src}" type="video/mp4"></video>
+  </a>
+</div>`
+  )
 }
