c01149a00d846476855d8ffde50bbe28b5404cfc
Garrett Sundin <gsundin@punchthrough.com>
2016-10-28 15:17:49 -0500

    Cleaned the code up a little bit

--- scripts/plugins/path-collector.js
+++ scripts/plugins/path-collector.js
@@ -23,26 +23,22 @@ module.exports = config => {
       return a.order - b.order
     }
 
-    // Permanent sidebar links for all subpages of /example-projects/ [WORKING]
+    // Permanent sidebar links for all subpages of /example-projects/
     let projects = config.projects
     let projectCollections = []
     projects.forEach((coll) => {
       projectCollections.push({key: coll.key, name: coll.name, pages: []})
-      //console.log("projectCollection: " + coll.name)
     })
 
-    // Permanent sidebar links for all subpages of /guides/ [WORKING]
+    // Permanent sidebar links for all subpages of /guides/
     let guides = config.guides
     let guideCollections = []
     guides.forEach((coll) => {
       guideCollections.push({key: coll.key, name: coll.name, pages: []})
-      //console.log("guidesCollection: " + coll.name)
     })
 
     // If files are in guides or example-projects
     Object.keys(files).forEach((filePath) => {
-      if (filePath.includes("guides")) {
-        console.log("GUIDES: " + filePath + " in " + Object.directory)
         let data = files[filePath]
         guides.forEach((coll, index) => {
           let collRegex = new RegExp(coll.pattern)
@@ -52,10 +48,6 @@ module.exports = config => {
             files[filePath].coll_key = coll.key
           }
         })
-      }
-      else if (filePath.includes("example-projects")) {
-        console.log("PROJECTS: " + filePath + " in " + Object.directory)
-        let data = files[filePath]
         projects.forEach((coll, index) => {
           let collRegex = new RegExp(coll.pattern)
           if (collRegex.test(filePath)) {
@@ -64,36 +56,30 @@ module.exports = config => {
             files[filePath].coll_key = coll.key
           }
         })
-      }
     })
 
-    console.log("SORTING THE GUIDES SUBDIRECTORY PATHS")
     // Sort all pages by their "order" property, if it exists
     guideCollections.forEach((coll) => {
       coll.pages = coll.pages.sort(pageOrder)
     })
     
-    console.log("SORTING THE PROJECTS SUBDIRECTORY PATHS\n")
-    // Sort all pages by their "order" property, if it exists
     projectCollections.forEach((coll) => {
       coll.pages = coll.pages.sort(pageOrder)
     })
 
     // Store in Metalsmith global object
     metalsmith.collections = guideCollections + projectCollections
+    
     // Store in each File for local access
     Object.keys(files).forEach((filePath) => {
       if (filePath.includes("guides")) {
       	files[filePath].collections = guideCollections
-      	//console.log("Put " + filePath + " in guideCollections")
       }
       else if (filePath.includes("projects")) {
       	files[filePath].collections = projectCollections
-      	//console.log("Put " + filePath + " in projectCollections")
       }
     })
     
-	console.log("There were " + count + ' ' + pluralize('file', count))
     done()
   }
 }
