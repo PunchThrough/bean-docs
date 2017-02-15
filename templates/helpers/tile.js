c423966071c73d69075efb442a8ee87efa2c17de
Garrett Sundin <gsundin@punchthrough.com>
2017-02-15 13:29:16 -0600

    fix: Changed indentations to pass CircleCI

--- templates/helpers/tile.js
+++ templates/helpers/tile.js
@@ -3,25 +3,24 @@
 let S = require('string')
 
 module.exports = (context, imagePath, desc, projectLink) => {
-    let srcTemplate = '{{relativeRoot}}_assets/images/projects/{{imagePath}}'
-    let htmlTemplate = ('<div class="guide-tile-holder bg">' +
-                        '<div class="crop">' +
-                        '<a href="{{projectLink}}" target="_blank">' +
-                        '<img class="guide-img" src="{{src}}" alt="{{desc}}" title="{{desc}}" ' +
-                        '</a>' +
-                        '<div class="overlay"> <h2>{{desc}}</h2> </div>' +
-                        //'style=""' +
-                        '</div>' +
-                        '</div>')
+  let srcTemplate = '{{relativeRoot}}_assets/images/projects/{{imagePath}}'
+  let htmlTemplate = ('<div class="guide-tile-holder bg">' +
+                      '<div class="crop">' +
+                      '<a href="{{projectLink}}" target="_blank">' +
+                      '<img class="guide-img" src="{{src}}" alt="{{desc}}" title="{{desc}}" ' +
+                      '</a>' +
+                      '<div class="overlay"> <h2>{{desc}}</h2> </div>' +
+                      '</div>' +
+                      '</div>')
 
-    let src = S(srcTemplate).template({
-        relativeRoot: context.relativeRoot,  // Relative root (e.g. './')
-        imagePath: imagePath,  // Actual filename of image (e.g. 'birbs.jpg')
-    })
+  let src = S(srcTemplate).template({
+      relativeRoot: context.relativeRoot,  // Relative root (e.g. './')
+      imagePath: imagePath  // Actual filename of image (e.g. 'birbs.jpg')
+  })
 
-    return S(htmlTemplate).template({
-        src: src,
-        desc: desc,
-        projectLink: projectLink  // Link to the project page
-    })
+  return S(htmlTemplate).template({
+      src: src,
+      desc: desc,
+      projectLink: projectLink  // Link to the project page
+  })
 }
